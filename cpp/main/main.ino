eb48767fc667757ff5cf6ae17bbc965a8bc60782
Arnav Bansal <arnav.bansal95@gmail.com>
2020-05-26 18:07:49 +0200

    Removed Relay Test

--- cpp/main/main.ino
+++ cpp/main/main.ino
@@ -15,40 +15,5 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  if(digitalRead(INPUT_SP1) == LOW)
-  {
-    digitalWrite(OUTPUT2_SP1, HIGH);
-    digitalWrite(OUTPUT2_SP2, LOW);
-    digitalWrite(OUTPUT2_DIN, LOW);
-    digitalWrite(OUTPUT2_DNM, LOW);
-    digitalWrite(OUTPUT2_UPM, LOW);
-    digitalWrite(OUTPUT2_MCN, HIGH);
-  }
-  if(digitalRead(INPUT_ESP) == LOW)
-  {
-    digitalWrite(OUTPUT2_SP1, LOW);
-    digitalWrite(OUTPUT2_SP2, HIGH);
-    digitalWrite(OUTPUT2_DIN, LOW);
-    digitalWrite(OUTPUT2_DNM, LOW);
-    digitalWrite(OUTPUT2_UPM, HIGH);
-    digitalWrite(OUTPUT2_MCN, LOW);
-  }
-  if(digitalRead(INPUT_RST) == LOW)
-  {
-    digitalWrite(OUTPUT2_SP1, LOW);
-    digitalWrite(OUTPUT2_SP2, LOW);
-    digitalWrite(OUTPUT2_DIN, HIGH);
-    digitalWrite(OUTPUT2_DNM, HIGH);
-    digitalWrite(OUTPUT2_UPM, LOW);
-    digitalWrite(OUTPUT2_MCN, LOW);
-  }
-  if(digitalRead(INPUT_OLR) == LOW)
-  {
-    digitalWrite(OUTPUT2_SP1, HIGH);
-    digitalWrite(OUTPUT2_SP2, HIGH);
-    digitalWrite(OUTPUT2_DIN, HIGH);
-    digitalWrite(OUTPUT2_DNM, HIGH);
-    digitalWrite(OUTPUT2_UPM, HIGH);
-    digitalWrite(OUTPUT2_MCN, HIGH);
-  }
+  
 }
