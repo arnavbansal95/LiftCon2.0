5daac747f0d42f05198ac47ba60d8f2ccfc02b24
Arnav Bansal <arnav.bansal95@gmail.com>
2020-05-26 18:10:05 +0200

    Power-Down Outputs at Start

--- cpp/main/src/outputs.cpp
+++ cpp/main/src/outputs.cpp
@@ -5,9 +5,11 @@ void InitOutputs()
     for(int i=2;i<14;i++)
     {
         pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);
     }
     for(int i=23;i<34;i=i+2)
     {
         pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);
     }
 }
