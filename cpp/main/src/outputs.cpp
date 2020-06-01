bb77da3058078a256f583ea7d0b91aaee8b88523
Arnav Bansal <arnav.bansal95@gmail.com>
2020-06-01 12:40:06 +0200

    Updated LCD Library Structure

--- cpp/main/src/outputs.cpp
+++ cpp/main/src/outputs.cpp
@@ -34,8 +34,5 @@ void InitLCD()
         LCD.setHiColorByRGB(255,255,255);
     }
     LCD.setFont(u8g_font_unifont);
-    Serial.println("Printing on LCD");
-    LCD.drawStr( 0, 20, "ARNAV BANSAL");
-    delay(2000);
-    Serial.println("Done");
+    LCD.drawStr( 0, 22, "ARNAV BANSAL");
 }
