f175399dd1b1f3d50f2495b85abc8b9179a6b3de
Arnav Bansal <arnav.bansal95@gmail.com>
2020-06-01 13:21:18 +0200

    Updated InitLCD Function (Test 5)

--- cpp/main/src/outputs.cpp
+++ cpp/main/src/outputs.cpp
@@ -17,22 +17,7 @@ void InitOutputs()
 void InitLCD()
 {
     // assign default color value
-    if(u8g.getMode() == U8G_MODE_R3G3B2) 
-    {
-        u8g.setColorIndex(255);     // white
-    }
-    else if(u8g.getMode() == U8G_MODE_GRAY2BIT) 
-    {
-        u8g.setColorIndex(3);         // max intensity
-    }
-    else if(u8g.getMode() == U8G_MODE_BW) 
-    {
-        u8g.setColorIndex(1);         // pixel on
-    }
-    else if(u8g.getMode() == U8G_MODE_HICOLOR) 
-    {
-        u8g.setHiColorByRGB(255,255,255);
-    }
+    u8g.firstPage();
     u8g.setFont(u8g_font_6x10);
     u8g.setFontRefHeightExtendedText();
     u8g.setDefaultForegroundColor();
