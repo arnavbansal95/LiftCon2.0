2e96bdfd35d05fd7ae8e8626609604d9d5f37c49
Arnav Bansal <arnav.bansal95@gmail.com>
2020-06-02 11:58:45 +0200

    Code Rearranged - TaskScheduler

--- cpp/main/src/task.h
+++ cpp/main/src/task.h
@@ -1,9 +1,15 @@
-
+#include "TaskSchedulerDeclarations.h"
 #include "outputs.h"
 #include "inputs.h"
 #include "comms.h"
 
 #ifndef TASK_H_
 #define TASK_H_
+
+void CheckUPDownManual(void);
+
+static Task CheckUPDownManualTask(10, TASK_FOREVER, &CheckUPDownManual);
+static Task SplashScreenTask(1, 5000, &SplashScreen);
+static Scheduler runner;
 
 #endif
