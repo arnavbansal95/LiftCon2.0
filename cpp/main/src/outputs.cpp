7443e742ff4f25411f2a56e6c86f9774efbae801
Arnav Bansal <arnav.bansal95@gmail.com>
2020-08-01 19:17:00 +0200

    Logging in OutputMotion

--- cpp/main/src/outputs.cpp
+++ cpp/main/src/outputs.cpp
@@ -18,16 +18,19 @@ void OutputMotion(motion_t smVar = HALT)
 {
     if((smVar == HALT) || (smVar == IDLE))
     {
+        Serial.println("HALT/IDLE");
         digitalWrite(OUTPUT2_UPM, LOW);
         digitalWrite(OUTPUT2_DNM, LOW);
     }
     if(smVar == UP)
     {
+        Serial.println("UP");
         digitalWrite(OUTPUT2_DNM, LOW);
         digitalWrite(OUTPUT2_UPM, (!ReadInput(INPUT_BUP) & !ReadInput(INPUT_RLU)));
     }
     if(smVar == DOWN)
     {
+        Serial.println("DOWN");
         digitalWrite(OUTPUT2_UPM, LOW);
         digitalWrite(OUTPUT2_DNM, (!ReadInput(INPUT_BDN) & !ReadInput(INPUT_RLD)));
     }
